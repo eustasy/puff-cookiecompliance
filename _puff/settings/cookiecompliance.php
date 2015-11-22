75dd456e383be5a37bce568b165d0441b1f1fee8
Lewis Goddard <lewisgoddard@eustasy.org>
2015-11-22 20:04:05 +0000

    Create cookiecompliance.php

---
+++ _puff/settings/cookiecompliance.php
@@ -0,0 +1,9 @@
+<?php
+
+////	Settings
+//
+// Do you want everyone to have to accept cookies?
+$Sitewide['Settings']['Cookies Compliance']['Worldwide'] = false;
+
+// Geo Version
+$Sitewide['Version']['CookieCompliance'] = '0.3';
