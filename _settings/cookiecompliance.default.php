71b4a4ab148fe6101641b9d81c04a234d665bf1b
Lewis Goddard <lewisgoddard@eustasy.org>
2016-02-14 13:06:05 +0000

    Update cookiecompliance.default.php

--- _settings/cookiecompliance.default.php
+++ _settings/cookiecompliance.default.php
@@ -1,11 +1,10 @@
 <?php
 
-////	Settings
+////	Puff Cookie Compliance Settings
 //
 // Do you want everyone to have to accept cookies?
 $Sitewide['Settings']['Cookies Compliance']['Worldwide'] = false;
 // Do you want to auto-accept?
 $Sitewide['Settings']['Cookies Compliance']['Auto Accept'] = false;
-
-// Geo Version
+// Version
 $Sitewide['Version']['CookieCompliance'] = '0.4';
