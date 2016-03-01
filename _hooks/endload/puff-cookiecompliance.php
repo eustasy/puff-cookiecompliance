33601dd536780f7cdcf86136a5149696dba88ef1
Lewis Goddard <lewisgoddard@eustasy.org>
2016-03-01 22:10:06 +0000

    Fix banner removal

--- _hooks/endload/puff-cookiecompliance.php
+++ _hooks/endload/puff-cookiecompliance.php
@@ -12,7 +12,7 @@ if (
 	) &&
 	!isset($_COOKIE[$CookieCompliance])
 ) {
-	echo <<<'SCRIPT1'
+	?>
 <script>
 var BodyTag = document.getElementsByTagName('body')[0];
 var CookieComplianceBanner = document.createElement('div');
@@ -21,15 +21,12 @@ CookieComplianceBanner.innerHTML = '<p>We use cookies on this site. <a class="cl
 BodyTag.insertBefore(CookieComplianceBanner, BodyTag.firstChild);
 document.getElementsByTagName('body')[0].className+=' has-cookie-compliance-banner';
 function removeMe() {
-SCRIPT1;
-	echo '
-	document.cookie = "'.$CookieCompliance.' = accepted; expires = Tues, 19 Jan 2038 03:14:07 UTC; path = /;";';
-	echo <<<'SCRIPT2'
-	var element = document.getElementById('cookie-compliance');
+	document.cookie = "<?php echo $CookieCompliance; ?> = accepted; expires = Tues, 19 Jan 2038 03:14:07 UTC; path = /;";
+	var element = document.getElementById('cookie-compliance-banner');
 	element.parentNode.removeChild(element);
 }
 </script>
-SCRIPT2;
+<?php
 
 } else if (
 	isset($_COOKIE[$CookieCompliance])
