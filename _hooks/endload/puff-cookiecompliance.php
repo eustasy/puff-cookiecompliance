ace3a343a02c097bf51f1ee40cff0e274cfdc296
Lewis Goddard <lewisgoddard@eustasy.org>
2016-03-10 01:39:32 +0000

    Update puff-cookiecompliance.php

--- _hooks/endload/puff-cookiecompliance.php
+++ _hooks/endload/puff-cookiecompliance.php
@@ -1,7 +1,4 @@
 <?php
-
-$CookieCompliance = $Sitewide['Cookies']['Prefix'].'_cookie_compliance';
-
 if (
 	(
 		!empty($Sitewide['Settings']['Cookies Compliance']['Worldwide']) ||
@@ -10,9 +7,9 @@ if (
 			$Sitewide['Geo']['Continent'] == 'EU'
 		)
 	) &&
-	!isset($_COOKIE[$CookieCompliance])
+	!isset($_COOKIE[$Sitewide['Settings']['Cookies Compliance']['Cookie']])
 ) {
-	?>
+?>
 <script>
 var BodyTag = document.getElementsByTagName('body')[0];
 var CookieComplianceBanner = document.createElement('div');
@@ -21,20 +18,10 @@ CookieComplianceBanner.innerHTML = '<p>We use cookies on this site. <a class="cl
 BodyTag.insertBefore(CookieComplianceBanner, BodyTag.firstChild);
 document.getElementsByTagName('body')[0].className+=' has-cookie-compliance-banner';
 function removeMe() {
-	document.cookie = "<?php echo $CookieCompliance; ?> = accepted; expires = Tues, 19 Jan 2038 03:14:07 UTC; path = /;";
+	document.cookie = "<?php echo $Sitewide['Settings']['Cookies Compliance']['Cookie']; ?> = accepted; expires = Tues, 19 Jan 2038 03:14:07 UTC; path = /;";
 	var element = document.getElementById('cookie-compliance-banner');
 	element.parentNode.removeChild(element);
 }
 </script>
 <?php
-
-} else if (
-	isset($_COOKIE[$CookieCompliance])
-) {
-	setcookie($CookieCompliance, 'accepted', 2147483647, '/', $Sitewide['Request']['Host'], $Sitewide['Request']['Secure'], $Sitewide['Cookies']['HTTPOnly']);
-
-} else if (
-	!empty($Sitewide['Settings']['Cookies Compliance']['Auto Accept'])
-) {
-	setcookie($CookieCompliance, 'auto-accepted', 2147483647, '/', $Sitewide['Request']['Host'], $Sitewide['Request']['Secure'], $Sitewide['Cookies']['HTTPOnly']);
 }
