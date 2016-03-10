a32ba2fbfe2f874d319cb1d06f2a2293d4aec4ec
Lewis Goddard <lewisgoddard@eustasy.org>
2016-03-10 01:38:33 +0000

    Update cookiecompliance.default.php

--- _settings/cookiecompliance.default.php
+++ _settings/cookiecompliance.default.php
@@ -6,6 +6,8 @@
 $Sitewide['Settings']['Cookies Compliance']['Worldwide'] = false;
 // Do you want to auto-accept?
 $Sitewide['Settings']['Cookies Compliance']['Auto Accept'] = false;
+// Cookie Name
+$Sitewide['Settings']['Cookies Compliance']['Cookie'] = $Sitewide['Cookies']['Prefix'].'_cookie_compliance';
 
 // Version
 $Sitewide['Version']['CookieCompliance'] = '0.4';
